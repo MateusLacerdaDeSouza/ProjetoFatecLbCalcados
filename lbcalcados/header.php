f4d133ee11d1af2ce1e63e0e6c9d06e1669009db
Mateus Lacerda <mateuslacerdadesouza120@gmail.com>
2024-10-29 19:58:19 -0300

    feat: primeira pagina da lb calcados front-end e prefixos primeiro commit

---
+++ lbcalcados/header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Header</title>
+    <link rel="stylesheet" href="css/header.css">
+</head>
+<body>
+    <header>
+        <div class="logo">
+            <img src="img/logo.png" alt="Logo da Marca" />
+        </div>
+        <div class="search-container">
+            <input type="text" placeholder="Procure seu sapato prefeito..." />
+            <img src="img/busca.png" alt="Busca" class="icon-busca">
+        </div>
+        <div class="icons">
+            <img src="img/image.png" alt="Carrinho de Compras" class="icon" /> <!-- Ícone do carrinho como imagem -->
+            <img src="img/perfil.png" alt="Perfil" class="icon"/>
+            <img src="img/carrinho.png" alt="Carrinho" class="icon"/>
+    </header>
+</body>
+</html>
+
