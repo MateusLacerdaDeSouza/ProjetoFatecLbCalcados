170c6d0912623d6304de4c3180fcafa993e97e84
Wesley Lopes <wesley.lopes0902@gmail.com>
2024-12-01 18:23:16 -0300

    eeee
    eeeee

---
+++ finalizar_compra.php
@@ -0,0 +1,18 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['cart'])) {
+    $cart = json_decode($_POST['cart'], true);
+
+    if ($cart) {
+        // mostras os produtos do carrinho
+        foreach ($cart as $item) {
+            echo "Cor: " . htmlspecialchars($item['color']) . "<br>";
+            echo "Tamanho: " . htmlspecialchars($item['size']) . "<br>";
+            echo "Quantidade: " . htmlspecialchars($item['quantity']) . "<br><br>";
+        }
+    } else {
+        echo "Carrinho está vazio!";
+    }
+} else {
+    echo "Nenhum dado recebido.";
+}
+?>
